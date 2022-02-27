3abd5118e006d55969470c09b5e994c8382f3147
SseBaaa <89750702+SseBaaa@users.noreply.github.com>
2022-02-27 19:29:46 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nasljedivanje_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+        }
+    }
+
+    public class Biljka
+    {
+
+    }
+
+    public class Stablo : Biljka
+    {
+        public bool otpadajuListovi;
+
+        public Stablo(bool otpadajuListovi)
+        {
+            otpadajuListovi = OtpadajuListovi;
+        }
+
+        public bool OtpadajuListovi { get => otpadajuListovi; set => otpadajuListovi = value; }
+    }
+
+    public class Cvijet : Biljka
+    {
+
+    }
+
+    public class Bjelogoricno : Stablo
+    {
+      public Bjelogoricno(bool otpadajuListovi)
+        {
+            otpadajuListovi = OtpadajuListovi;
+        }
+
+    }
+
+    public class Crnogoricno : Stablo
+    {
+        
+
+    }
+
+
+
+    public class Hrast : Bjelogoricno
+    {
+
+    }
+
+    public class Bor : Crnogoricno
+    {
+
+    }
+
+}
